77f7ee5cf494c27cbfef435903ad117c4879c2f7
yunusfirat <yunusfirat44@gmail.com>
2021-04-18 14:41:29 +0100

    Added bestPokemon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 
-function App() {
+function CaughtPokemon() {
+	let date = new Date().toLocaleDateString();
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-          Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-          Learn React
-				</a>
+		<div>
+			<p>Caught 0 Pokemon on {date}</p>
+		</div>
+	);
+}
+
+
+
+function Logo() {
+	let appName = "yunus`s pokedex";
+	return (
+		<div>
+			<header>
+				<h1>Welcome to the {appName}</h1>
+				<img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="pokedex"></img>
 			</header>
 		</div>
 	);
 }
+
+function BestPokemon() {
+	let abilities = ["Anticipation", "Adaptability", "Run-Away"];
+	return (
+		<p>My favorite Pokemon is Squirtle
+			<ul>
+				{abilities.map((ability) => {
+					return (
+						<li>{ability}</li>
+					);
+				})}
+			</ul>
+		</p>
+	);
+}
+function App() {
+	return (
+		<div>
+			<Logo />
+			<BestPokemon />
+			<CaughtPokemon />
+		</div>
+	);
+}
+
 
 export default App;
