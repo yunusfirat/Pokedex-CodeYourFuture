60f54a05cd2f9a6790424f305dc56df8a5a4ff5d
yunusfirat <yunusfirat44@gmail.com>
2021-04-25 16:03:39 +0100

    added random pokemon names and count

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -3,16 +3,27 @@ import React, { useState } from "react";
 
 
 function CaughtPokemon(props) {
-	const [caught, setCaught] = useState(0);
+	const [caught, setCaught] = useState([]);
 
 	const catchPokemon = () => {
-	 setCaught((caught) => caught + 1);
+	 let pokemonNames = ["pikachu","Butterfree","Ditto","Spearow"];
+	 let x = Math.floor((Math.random() * pokemonNames.length));
+	 setCaught(caught.concat(pokemonNames[x]));
 
 	};
+
+
 	return (
-		<div style= {{ display:"flex" } }>
-			<p>Caught {caught} Pokemon on {props.date}</p>
-			<button onClick={catchPokemon}>Catch Pokemon</button>
+		<div>
+			<p>Caught {caught.length} Pokemon on {props.date}</p>
+			<div><button onClick={catchPokemon}>Catch Pokemon</button></div>
+			<ul>
+				{caught.map((element, index) => {
+					return(
+						<li key={index}>{element}</li>
+					);
+				})}
+			</ul>
 		</div>
 	);
 }
