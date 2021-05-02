c101a62a17b59163fc1e92a47e10d70334938ed7
yunusfirat <yunusfirat44@gmail.com>
2021-05-02 16:00:29 +0100

    handle input change

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -4,18 +4,27 @@ import React, { useState } from "react";
 
 function CaughtPokemon(props) {
 	const [caught, setCaught] = useState([]);
+	const [pokemonNameInput, setpokemonNameInput] = useState("");
 
 	const catchPokemon = () => {
-	 let pokemonNames = ["pikachu","Butterfree","Ditto","Spearow"];
-	 let x = Math.floor((Math.random() * pokemonNames.length));
-	 setCaught(caught.concat(pokemonNames[x]));
+	//  let pokemonNames = ["pikachu","Butterfree","Ditto","Spearow"];
+	//  let x = Math.floor((Math.random() * pokemonNames.length));
+		pokemonNameInput ? setCaught(caught.concat(pokemonNameInput)) || setpokemonNameInput("") : alert("you can not add empty value");
 
 	};
+	const handleInputChange = (event) => {
+		setpokemonNameInput(event.target.value);
 
+	};
 
 	return (
 		<div>
 			<p>Caught {caught.length} Pokemon on {props.date}</p>
+			<input
+			 type="text"
+			 value={pokemonNameInput}
+			 onChange={handleInputChange}
+			 />
 			<div><button onClick={catchPokemon}>Catch Pokemon</button></div>
 			<ul>
 				{caught.map((element, index) => {
