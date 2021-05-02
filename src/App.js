a385d729d935cb6679047031c8afc569def495b6
yunusfirat <yunusfirat44@gmail.com>
2021-05-02 15:22:31 +0100

    added input

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Logo from "./Logo";
 import CaughtPokemon from "./CaughtPokemon";
 // import PokemonMoves from "./PokemonMoves";
 import PokemonMovesSelector from "./PokemonMovesSelector";
+import PokemonCity from "./PokemonCity";
 function App() {
 	let abilities = ["Anticipation", "Adaptability", "Run-Away"];
 	let date = new Date().toLocaleDateString();
@@ -17,6 +18,7 @@ function App() {
 			<BestPokemon abilities={abilities}/>
 			<CaughtPokemon  date={date}/>
 			<PokemonMovesSelector />
+			<PokemonCity />
 		</div>
 	);
 }
