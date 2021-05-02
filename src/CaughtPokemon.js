ea3268feb7b5ebb657c97061db19d66910e1ac2c
yunusfirat <yunusfirat44@gmail.com>
2021-05-03 00:40:49 +0100

    object state added to project

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -5,6 +5,9 @@ import React, { useState } from "react";
 function CaughtPokemon(props) {
 	const [caught, setCaught] = useState([]);
 	const [pokemonNameInput, setpokemonNameInput] = useState("");
+	const [pokemonObject, setPokemonObject] = useState({
+		pokemonname:"",
+	});
 
 	const catchPokemon = () => {
 	//  let pokemonNames = ["pikachu","Butterfree","Ditto","Spearow"];
@@ -12,9 +15,21 @@ function CaughtPokemon(props) {
 		pokemonNameInput ? setCaught(caught.concat(pokemonNameInput)) || setpokemonNameInput("") : alert("you can not add empty value");
 
 	};
+	const catchPokemonTwo = () => {
+		pokemonObject ? setCaught(caught.concat(pokemonObject)) || setPokemonObject("") : alert("you can not add empty value");
+	};
+
 	const handleInputChange = (event) => {
 		setpokemonNameInput(event.target.value);
 
+	};
+
+	const handleInputChangetwo = (event) => {
+		const updatedPokemon = {
+			...pokemonObject,
+			[event.target.name] : event.target.value,
+		};
+		setPokemonObject(updatedPokemon);
 	};
 
 	return (
@@ -25,11 +40,20 @@ function CaughtPokemon(props) {
 			 value={pokemonNameInput}
 			 onChange={handleInputChange}
 			 />
+			 <input
+			 type="text"
+			 name="pokemonname"
+			 placeholder="Pokemon name"
+			 value={pokemonObject.pokemonname}
+			 onChange={handleInputChangetwo}
+
+			 />
 			<div><button onClick={catchPokemon}>Catch Pokemon</button></div>
+			<div><button onClick={catchPokemonTwo}>Catch Pokemon two</button></div>
 			<ul>
 				{caught.map((element, index) => {
 					return(
-						<li key={index}>{element}</li>
+						<li key={index}>{typeof element === "string" ? element : element.pokemonname}</li>
 					);
 				})}
 			</ul>
